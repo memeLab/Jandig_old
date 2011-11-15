bfbeadd133d5d4c71f6e04d22062eca05b098bea
Thiago Hersan <thiago.hersan+github@gmail.com>
2011-11-15 00:46:21 -0200

    changed package name to com.memelab.jandig

--- gen/com/memelab/jandig/R.java
+++ gen/com/memelab/jandig/R.java
@@ -5,19 +5,21 @@
  * should not be modified by hand.
  */
 
-package edu.dhbw.andar.sample;
+package com.memelab.jandig;
 
 public final class R {
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int jicon=0x7f020001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        /**  <string name="hello">Hello World, MainActivity!</string> 
+         */
+        public static final int app_name=0x7f040000;
     }
 }
