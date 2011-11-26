f1a04c99e7b1af021c33b940405433a88f3775b3
pixel <pixel@memePiron.(none)>
2011-11-26 02:55:18 -0200

    changed version to 1.16

--- gen/com/memelab/jandig/R.java
+++ gen/com/memelab/jandig/R.java
@@ -13,7 +13,6 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int jicon=0x7f020001;
-        public static final int jicon00=0x7f020002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
